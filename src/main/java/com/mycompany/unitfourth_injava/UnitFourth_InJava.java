250d1fa59bbd1fa02b7ba1ce38100300f5fa899f
Axel-18hrs <Axel.18hrs@gmail.com>
2024-03-05 16:22:47 -0600

    first commit

---
+++ src/main/java/com/mycompany/unitfourth_injava/UnitFourth_InJava.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.unitfourth_injava;
+import Formularios.App;
+
+/**
+ *
+ * @author axel1
+ */
+public class UnitFourth_InJava {
+
+    public static void main(String[] args) {
+        App miFormulario = new App();
+        miFormulario.setVisible(true);
+    }
+}
